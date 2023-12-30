d9d426884961d996cf6dfb4be21a04ff75a5cb19
fakepixels <tinaxhe@gmail.com>
2023-12-30 05:01:35 -0500

    stylistic updates

--- blog.config.js
+++ blog.config.js
@@ -2,8 +2,8 @@ const BLOG = {
   title: 'TINY MIGHTY MODELS',
   author: 'Tina He',
   email: 'fkpxls@gmail.com',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  link: 'https://tiny-mighty-models.vercel.app/',
+  description: 'A comilation of tiny language models.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
